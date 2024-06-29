9d641b047f0bae11858d7c3f2f03db5ea8a8ce05
karwei <karwei2002@gmail.com>
2024-06-30 02:02:37 +0800

    done homepage

---
+++ frontend/src/assets/index.js
@@ -0,0 +1,6 @@
+import greylogo from './logo-grey.png';
+import whitelogo from './logo4.png';
+import solanalogo from './Solana.png'
+import foambg from './Foambg.svg'
+import foam from './foam2.png'
+export {whitelogo,greylogo,solanalogo,foambg,foam}
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { whitelogo,solanalogo,foambg,foam } from '../assets'
+import { IoWallet } from "react-icons/io5";
 
 const Homepage = () => {
   return (
-    <div>
-      <p className="text-red-500 text-[30px]">test</p>
+    <div className="flex flex-col items-center  bg-cover bg-center pt-[100px] "style={{backgroundImage:`url(${foam})`}}>
+      <div className=" md:w-[500px] w-[300px]">
+        <div className='items-center flex flex-col'>
+          <div>
+            <img src={whitelogo} className="md:w-[300px] w-[200px] mt-[50px] " />
+          </div>
+          {/* <div>
+            <p className="text-white font-bold text-[50px]">Sol-LaH</p>
+          </div> */}
+          <div className="bg-white rounded-3xl shadow-2xl p-4 mt-4 items-center flex flex-col w-[90%]">
+            <p className="md:text-[28px] text-[20px] font-semibold text-[#3A6073] text-center">You'll need a wallet on <br/> Solana to continue</p>
+            <div className='mb-8 mt-6'>
+              <img src={solanalogo}  className="md:h-[100px] h-[80px] "/>
+            </div>
+            <div className='mb-4'>
+            <button className="flex items-center text-[#3A6073] md:text-[20px] text-[18px] rounded-full  pl-4 pr-3 py-2 font-semibold border-4 border-[#3A6073] hover:border-[#00C000] hover:text-[#00C000]  hover:text-white group gradient-ring-button">Connect to a wallet <IoWallet className='ml-2 text-[#3A6073] group-hover:text-[#00C000]' /></button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
