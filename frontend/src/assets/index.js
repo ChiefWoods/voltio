9119a123de02cfa7b9d8136a6363f8f3f634d0bc
eesuhn <eason.yihong@gmail.com>
2024-07-04 13:09:06 +0800

    Added assets

---
+++ frontend/src/assets/index.js
@@ -0,0 +1,15 @@
+import token_voltio_png from './token_voltio.png';
+import token_voltio_svg from './token_voltio.svg';
+import voltio_logo_gradient_bg from './voltio_logo_gradient_bg.png';
+import voltio_logo_white_bg from './voltio_logo_white_bg.png';
+import voltio_logo_png from './voltio_logo.png';
+import voltio_logo_svg from './voltio_logo.svg';
+
+export {
+  token_voltio_png,
+  token_voltio_svg,
+  voltio_logo_gradient_bg,
+  voltio_logo_white_bg,
+  voltio_logo_png,
+  voltio_logo_svg,
+}
