dbfd0d40588eb2126d496df6b971cf4015e13a27
jian <salivasauce@gmail.com>
2024-07-05 16:44:59 +0800

    added wallet connection check to faucet

--- frontend/src/pages/Faucet.jsx
+++ frontend/src/pages/Faucet.jsx
@@ -1,34 +1,44 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
   BackgroundImage,
   TopBar,
 } from '../components';
 
+import { useWallet } from '@solana/wallet-adapter-react';
+
 
 const Faucet = () => {
+	const { publicKey } = useWallet();
+
 	return (
 	<BackgroundImage>
 		<TopBar currentPage='Voltio Faucet'/>
-
-		<div className="bg-[#1e1e1e] p-10 rounded-[50px] mt-10">
-			<form>
-			<div className="mb-4">
-				<input
-				type="text"
-				className="w-full px-4 py-2 text-black rounded"
-				placeholder="Number"
-				/>
-			</div>
-			<div>
-				<button
-				type="submit"
-				className="w-full px-4 py-2 text-white bg-blue-500 rounded"
-				>
-				Submit
-				</button>
-			</div>
-			</form>
-		</div>
+		{
+			!publicKey
+				?
+					<h1>Connect to a wallet or risk ▇▇▇▇</h1>
+				:
+					<div className="bg-[#1e1e1e] p-10 rounded-[50px] mt-10">
+						<form>
+							<div className="mb-4">
+								<input
+								type="text"
+								className="w-full px-4 py-2 text-black rounded"
+								placeholder="Number"
+								/>
+							</div>
+							<div>
+								<button
+									type="submit"
+									className="w-full px-4 py-2 text-white bg-blue-500 rounded"
+									>
+									Submit
+								</button>
+							</div>
+						</form>
+					</div>
+		}
+		
 	  </BackgroundImage>
 
 	);
